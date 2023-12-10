25eff81afdb277f7fecfc4bcc9fa2162595bf7a6
ToKaKarTsivadze <tokaqarco2001@gmail.com>
2023-12-11 00:34:31 +0400

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>amazona</header>
+      <main>items will be listed here</main>
+      <footer>all rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
